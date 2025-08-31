3229af7a106928cb32a197f4599bf8b9370ec35f
MD.ASAD-UZ-ZAMAN <129204973+Asad-mim@users.noreply.github.com>
2025-08-31 23:46:13 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const apikey = "a528be75640c27605f0c8d74aacf06c8";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+    var data = await response.json();
+    
+
+    if (data.cod === "404") {   
+        alert("City not found!");
+        return;
+    }
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    var icon = data.weather[0].icon;
+    document.querySelector(".weather-icon").src =
+        "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".weather").style.display = "block";
+}
+
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+
+searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+});
